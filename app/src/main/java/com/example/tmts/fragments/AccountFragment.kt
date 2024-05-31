b44118eb068d03189d6e55df852426f2dfbb6b8b
Raul Cocis <raul.cocis27@gmail.com>
2024-05-31 17:00:07 +0200

    Modifiche colori + aggiunta PopUpMenu
    Da risolvere: displayName = stringa vuota o null (se già loggato oppure con login in-app)

--- app/src/main/java/com/example/tmts/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/tmts/fragments/AccountFragment.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
 import com.example.tmts.R
 import com.example.tmts.activities.MainEmptyActivity
@@ -25,25 +28,49 @@ class AccountFragment : Fragment() {
         mAuth = FirebaseAuth.getInstance()
         val currentUser = mAuth.currentUser
 
-        val greetingTextView: TextView = view.findViewById(R.id.account_fragment_saluto)
+        //val greetingTextView: TextView = view.findViewById(R.id.account_fragment_saluto)
         val tvUsername: TextView = view.findViewById(R.id.tv_account_username)
-        val logoutButton: Button = view.findViewById(R.id.logout_button)
+        //val logoutButton: Button = view.findViewById(R.id.logout_button2)
+        val ibDropDown: ImageButton = view.findViewById(R.id.ib_dropdown)
 
         //Quando si entra in app direttamente (senza login): name = ""
         // mentre con login, name = "null"
 
-        greetingTextView.text = "Ciao, ${currentUser?.displayName}"
-        tvUsername.text = "${currentUser?.displayName}"
+        //greetingTextView.text = "Ciao, ${currentUser?.displayName}"
+        //tvUsername.text = "${currentUser?.displayName}"
 
-        logoutButton.setOnClickListener {
-            performLogout()
+//        logoutButton.setOnClickListener {
+//            performLogout()
+//        }
+
+        ibDropDown.setOnClickListener {
+            val popup = PopupMenu(requireContext(), it)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.dropdown_menu, popup.menu)
+            popup.show()
+            popup.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.edit_profile -> editProfile()
+                    R.id.logout_button -> performLogout()
+                }
+                true
+            }
         }
+
         return view
     }
 
-    private fun performLogout() {
+    private fun editProfile(): Boolean {
+        //TODO: to be implemented
+        return true
+    }
+
+    private fun performLogout(): Boolean {
         mAuth.signOut()
         val intent = Intent(requireContext(), MainEmptyActivity::class.java)
         startActivity(intent)
+        return true
     }
+
+
 }
