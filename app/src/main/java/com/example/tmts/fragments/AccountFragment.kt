fb6ae53dfd6bb90e3744fc424cb3bc0abe0cd2f6
Raul Cocis <raul.cocis27@gmail.com>
2024-06-18 14:52:31 +0200

    Fixato upload su firebase
    Aggiunta classe MyAppGlideModule che server esplicitamente per funzionamento fra FirebaseStorage e Glide, serve usare GlideApp!

--- app/src/main/java/com/example/tmts/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/tmts/fragments/AccountFragment.kt
@@ -3,34 +3,37 @@ package com.example.tmts.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.ContextMenu
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.tmts.R
 import com.example.tmts.activities.MainEmptyActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.storage
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+
 
 class AccountFragment : Fragment() {
-    private lateinit var mAuth: FirebaseAuth
+    private var mDbRef = FirebaseDatabase.getInstance().getReference()
+    private var mAuth = FirebaseAuth.getInstance()
+    private var mStorage = FirebaseStorage.getInstance().getReference()
     private lateinit var ivAccountIcon: ImageView
-    lateinit var imagesRef: StorageReference
+
+    val currentUser = mAuth.currentUser!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,13 +41,23 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_account, container, false)
+        val userRef = mDbRef.child("users").child(currentUser.uid)
 
-        mAuth = FirebaseAuth.getInstance()
-        val currentUser = mAuth.currentUser
-        var fStorage = FirebaseStorage.getInstance()
-
-        //val greetingTextView: TextView = view.findViewById(R.id.account_fragment_saluto)
         val tvUsername: TextView = view.findViewById(R.id.tv_account_username)
+
+        userRef.child("name").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val name = snapshot.getValue(String::class.java)
+                tvUsername.text = "$name"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        val userImageRef = mStorage.child("users").child(currentUser.uid).child("profileImage")
+
         //val logoutButton: Button = view.findViewById(R.id.logout_button2)
         val ibDropDown: ImageButton = view.findViewById(R.id.ib_dropdown)
 
@@ -52,7 +65,7 @@ class AccountFragment : Fragment() {
         // mentre con login, name = "null"
 
         //greetingTextView.text = "Ciao, ${currentUser?.displayName}"
-        //tvUsername.text = "${currentUser?.displayName}"
+
 
 //        logoutButton.setOnClickListener {
 //            performLogout()
@@ -61,6 +74,7 @@ class AccountFragment : Fragment() {
         //TODO: Fare in modo che qui ivAccountIcon venga impostata a quello che è stato caricato su Firebase, se presente
 
         ivAccountIcon = view.findViewById(R.id.account_icon)
+        loadUserImage(userImageRef, ivAccountIcon)
 
         ivAccountIcon.setOnClickListener{
             val popup = PopupMenu(requireContext(), it)
@@ -109,31 +123,45 @@ class AccountFragment : Fragment() {
         return true
     }
 
-    private fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
+    private fun selectImageFromGallery() {
+        selectImageFromGalleryResult.launch("image/*")
+    }
 
     private val selectImageFromGalleryResult = registerForActivityResult(ActivityResultContracts.GetContent()) {
 
             uri: Uri? -> uri?.let {
-        val formatter = SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.getDefault())
-        var now = Date()
-        val filename = formatter.format(now)
+            //TODO: Se faccio upload di 1 immagine, poi switch fragment, poi ritorno a fare upload immagine, mi porta a un crash
 
-        //TODO: Scommentare per garantire upload su firebase. Cambiare immagine SOLO se upload avviene con successo
-        // Perchè se avviene fallimento, l'immagine nuova sarà caricata solo localmente e quindi:
-        // 1. Al prossimo avvio forse non l'avrà
-        // 2. Non posso rimettere l'immagine precedente perchè magari in locale non la ho più
+            val userImageRef = mStorage.child("users").child(currentUser.uid).child("profileImage")
 
-        val uid = mAuth.uid.toString()
-
-        imagesRef = FirebaseStorage.getInstance().getReference("users/$uid/profilePicture/$filename")
-        imagesRef.putFile(uri).addOnSuccessListener {
-            Toast.makeText(requireContext(), "Uploaded successfully!", Toast.LENGTH_SHORT).show()
-            ivAccountIcon.setImageURI(uri)
-        }.addOnFailureListener {
-            Toast.makeText(requireContext(), "Something went wrong!", Toast.LENGTH_SHORT).show()
+            userImageRef.putFile(uri).addOnSuccessListener {
+                Toast.makeText(requireContext(), "Uploaded successfully!", Toast.LENGTH_SHORT).show()
+                ivAccountIcon.setImageURI(uri)
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "Something went wrong!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
+    private fun loadUserImage(userImageRef: StorageReference, ivAccountIcon: ImageView) {
+        userImageRef.downloadUrl.addOnSuccessListener {
+            // File exists, load with Glide
+            Glide.with(requireContext())
+                .load(userImageRef)
+                .placeholder(R.drawable.movie)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .into(ivAccountIcon)
+        }.addOnFailureListener{
+            // File doesn't exist, load default image
+            ivAccountIcon.setImageResource(R.drawable.movie) // TODO: change default image
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        selectImageFromGalleryResult.unregister()
     }
 
 }
