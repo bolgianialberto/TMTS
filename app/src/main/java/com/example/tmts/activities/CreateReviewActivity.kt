f00cc042d3bf9533ef1644cd4b5fb1091a0321fc
Alberto Bolgiani <a.bolgiani@studenti.unibs.it>
2024-06-19 18:14:40 +0200

    small changes

--- app/src/main/java/com/example/tmts/activities/CreateReviewActivity.kt
+++ app/src/main/java/com/example/tmts/activities/CreateReviewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tmts.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -9,12 +10,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tmts.FirebaseInteraction
+import com.example.tmts.MediaRepository
 import com.example.tmts.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CreateReviewActivity : AppCompatActivity() {
     private lateinit var btnClose: Button
     private lateinit var btnPost: Button
     private lateinit var etReview: EditText
+    private lateinit var btnAddPhoto: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_review)
@@ -24,6 +28,17 @@ class CreateReviewActivity : AppCompatActivity() {
         btnClose = findViewById(R.id.btn_close)
         btnPost = findViewById(R.id.btn_post)
         etReview = findViewById(R.id.et_review)
+        btnAddPhoto = findViewById(R.id.fab_add_photo)
+
+        MediaRepository.getMovieDetails(
+            movieId,
+            onSuccess = {movie ->
+                etReview.hint = "Write your review about ${movie.title}"
+            },
+            onError = {
+                Log.e("CreateReviewActivity", "Something went wrong")
+            }
+        )
 
         btnClose.setOnClickListener {
             finish()
