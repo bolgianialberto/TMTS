a1895a434f4c267cc967d4e5c099488f46e5670b
Claudio Metelli <metelli.claudio@gmail.com>
2024-06-19 12:12:21 +0200

    Added fetch on following movies in explore

--- app/src/main/java/com/example/tmts/fragments/ExploreFragment.kt
+++ app/src/main/java/com/example/tmts/fragments/ExploreFragment.kt
@@ -9,16 +9,6 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.tmts.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreFragment : Fragment() {
     private lateinit var bttExplore: Button
     private lateinit var bttChat: Button
@@ -70,25 +60,5 @@ class ExploreFragment : Fragment() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fl_explore_fragment, fragment)
         fragmentTransaction.commit()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExploreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExploreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- app/src/main/java/com/example/tmts/fragments/ExploreMoviesFragment.kt
+++ app/src/main/java/com/example/tmts/fragments/ExploreMoviesFragment.kt
@@ -1,60 +1,74 @@
 package com.example.tmts.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tmts.FirebaseInteraction
+import com.example.tmts.FirebaseInteraction.onError
+import com.example.tmts.MediaRepository
 import com.example.tmts.R
+import com.example.tmts.adapters.ExploreMovieAdapter
+import com.example.tmts.beans.MovieDetails
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreMoviesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreMoviesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var rvExplore: RecyclerView
+    private lateinit var exploreMoviesAdapter: ExploreMovieAdapter
+    val movieDetailsList = mutableListOf<Pair<MovieDetails, Long>>()
+    val followingMovies = mutableListOf<Pair<String, Long>>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore_movies, container, false)
+        val view = inflater.inflate(R.layout.fragment_explore_movies, container, false)
+        exploreMoviesAdapter = ExploreMovieAdapter(requireContext(), emptyList())
+        rvExplore = view.findViewById(R.id.rv_explore_movie)
+        rvExplore.layoutManager = LinearLayoutManager(requireContext())
+        rvExplore.adapter = exploreMoviesAdapter
+        loadMovies()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExploreMoviesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExploreMoviesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun loadMovies() {
+        movieDetailsList.clear()
+        followingMovies.clear()
+        FirebaseInteraction.getFollowingMovies { movies ->
+            followingMovies.addAll(movies)
+            fetchDetails()
+        }
+    }
+
+    private fun fetchDetails() {
+        for (movieId in followingMovies) {
+            MediaRepository.getMovieDetails(
+                movieId.first.toInt(),
+                onSuccess = ::onMovieDetailsFetched,
+                onError = ::onError)
+        }
+        if (followingMovies.isEmpty()) {
+            exploreMoviesAdapter.updateMovies(emptyList())
+            Log.d("FetchMovieDetails", "Nessun film da elaborare.")
+        }
+    }
+
+    private fun onMovieDetailsFetched(movie: MovieDetails){
+        val pair = followingMovies.find { it.first == movie.id.toString() }
+        val timestamp = pair!!.second
+
+        movieDetailsList.add(Pair(movie, timestamp))
+
+        if (movieDetailsList.size == followingMovies.size) {
+            val sortedList = movieDetailsList.sortedBy { it.second }
+            val sortedMovies = sortedList.map { it.first }
+
+            exploreMoviesAdapter.updateMovies(sortedMovies)
+        }
     }
 }
