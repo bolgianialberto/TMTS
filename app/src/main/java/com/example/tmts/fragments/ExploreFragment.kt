e89ee042833a5c93cee7a69c22614d2730c3556f
Claudio Metelli <metelli.claudio@gmail.com>
2024-06-19 09:09:50 +0200

    This commit is a test

--- app/src/main/java/com/example/tmts/fragments/ExploreFragment.kt
+++ app/src/main/java/com/example/tmts/fragments/ExploreFragment.kt
@@ -2,16 +2,14 @@ package com.example.tmts.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.example.tmts.MediaDetaisActivity
+import androidx.fragment.app.Fragment
 import com.example.tmts.R
 import com.example.tmts.adapters.ExploreAdapter
-import com.example.tmts.adapters.MediaAdapter
-import com.example.tmts.beans.User
+import com.example.tmts.beans.Media
 
 class ExploreFragment : Fragment() {
     private lateinit var exploreAdapter: ExploreAdapter
@@ -31,7 +29,7 @@ class ExploreFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_explore, container, false)
         bttExploreSearch = view.findViewById(R.id.btt_search_explore)
         exploreAdapter = ExploreAdapter(requireContext(), emptyList()) { exploreItem ->
-            val intent = Intent(requireContext(), User::class.java)
+            val intent = Intent(requireContext(), Media::class.java)
             intent.putExtra("userId", exploreItem.id)
             startActivity(intent)
         }
